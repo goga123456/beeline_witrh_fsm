b552c99255ebc8a2c8fac2538101c51b1df41d4c
goga123456 <bukanov1234@mail.ru>
2023-02-28 14:01:36 +0500

    fsm

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn --workers=1 core.wsgi
+clock: python bot/scheduller.py
---
+++ bot/config.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+token = "5875923517:AAHdupc3iddCYl-5XYQj79ZeCeU80cx28XU"
+db_file = "database.vdb"
+
+
+class States(Enum):
+    """
+    Мы используем БД Vedis, в которой хранимые значения всегда строки,
+    поэтому и тут будем использовать тоже строки (str)
+    """
+    S_START = "0"  # Начало нового диалога
+    S_ENTER_first_text= "1"
+    S_ENTER_second_text = "2"
+    S_ENTER_number = "3"
+    S_ENTER_name = "4"
+    S_ENTER_surname = "5"
+    S_ENTER_birthday = "6"
+    S_ENTER_day = "7"
+    S_ENTER_month = "8"
+    S_ENTER_year = "9"
+    S_ENTER_where_are_you_from = "10"
+    S_ENTER_district = "11"
+    S_ENTER_town_and_district = "12"
+    S_ENTER_edu = "13"
+    S_ENTER_uzb = "14"
+    S_ENTER_ru = "15"
+    S_ENTER_eng = "16"
+    S_ENTER_work_experience = "17"
+    S_ENTER_work_experience_yes = "18"
